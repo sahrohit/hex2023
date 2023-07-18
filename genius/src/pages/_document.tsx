747eaaac0c1b618bbe52270c05b7a076a0859ee0
Rohit Sah <sahrohit9586@gmail.com>
2023-07-18 11:48:36 +0545

    added google analytics

--- genius/src/pages/_document.tsx
+++ genius/src/pages/_document.tsx
@@ -1,4 +1,5 @@
-import { Head, Html, Main, NextScript } from 'next/document'
+import { Head, Html, Main, NextScript } from "next/document"
+import Script from "next/script"
 
 export default function Document() {
   return (
@@ -13,6 +14,24 @@ export default function Document() {
         <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=DM+Sans:wght@400;500;700&display=swap"
+        />
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=G-CYWXRXNYN9`}
+        />
+        <Script
+          id="gtag-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-CYWXRXNYN9', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
         />
       </Head>
       <body>
